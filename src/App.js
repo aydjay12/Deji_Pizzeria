a740c7890636e57fb892bffee7f64abf503022dd
olalekan github <aydjay.gmail.net>
2023-11-16 12:03:56 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar></Navbar>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/menu" element={<Menu></Menu>}></Route>
+          <Route path="/about" element={<About></About>}></Route>
+          <Route path="/contact" element={<Contact></Contact>}></Route>
+        </Routes>
+        <Footer></Footer>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
